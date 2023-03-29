9dda60b75cca83008adc8fe7a08398d7135bd012
Liam Pieri <liamwazherealso@gmail.com>
2023-03-29 14:19:41 -0400

    Pivot to lambda

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run:
 	poetry run python src/news_ml_scraper/scraper.py
+lambda:
+	pip install -t dist/lambda .
+	cd dist/lambda && zip -x '*.pyc' -r ../lambda.zip .
--- news_ml_scraper/scraper.py
+++ news_ml_scraper/scraper.py
@@ -1,15 +1,11 @@
 import json
 import logging
-import os
 import sys
 from collections.abc import Iterator
 from datetime import date, datetime, timedelta
-from pathlib import Path
 
 import boto3
-import click
 import newspaper
-from dotenv import find_dotenv, load_dotenv
 from gnews import GNews
 
 logging.basicConfig(
@@ -19,13 +15,16 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
 DATE_FMT_STR = "%Y-%m-%d"
 
+config = {}
 
-def s3_put_object(art_date: str, art_topic: str, art_tile: str, art_dict: dict):
+
+def s3_put_object(
+    art_date: str, art_topic: str, art_tile: str, art_dict: dict
+):
     s3 = boto3.resource("s3")
-    s3.Bucket(os.environ["S3_BUCKET"]).put_object(
+    s3.Bucket(config["S3_BUCKET"]).put_object(
         Key=f"""{art_date}/{art_topic}/
                                     {art_tile}""",
         Body=json.dumps(art_dict),
@@ -102,7 +101,7 @@ def get_articles_for_date_range(date_range: Iterator[date]):
                 date_obj = datetime.strptime(date_str, format_str)
                 article["published date"] = date_obj.strftime(DATE_FMT_STR)
 
-                # A current (03/29/2023) limitation of the GNews library is that
+                # (03/29/2023) limitation of the GNews library is that
                 # it cannot filter by date for the "get_news_by_topic" method.
                 # This is a workaround to filter by date.
 
@@ -130,23 +129,18 @@ def get_articles_for_date_range(date_range: Iterator[date]):
     logger.info("making final data set from raw data")
 
 
-@click.command()
 def main():
-    start_date = datetime.strptime(os.environ["START_DATE"], DATE_FMT_STR)
-    end_date = datetime.now()
+    end_date = datetime.today()
+    start_date = end_date - timedelta(days=1)
     date_range = daterange(start_date, end_date)
 
+    # TODO: Breakout period into event option once testing is complete
     get_articles_for_date_range(date_range)
 
 
-if __name__ == "__main__":
+def lambda_handler(event, context):
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    config["S3_BUCKET"] = event["S3_BUCKET"]
     main()
